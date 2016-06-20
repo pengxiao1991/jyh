a901d1d55c797ca45dca90a8f58bd728ef5d38fc
pengxiao1991 <1337565590@qq.com>
2016-06-21 00:51:15 +0800

    首页头部初步完成

--- js/common/common.js
+++ js/common/common.js
@@ -1,5 +1,6 @@
 //头部js
 (function () {
+	//顶部导航的二维码的hover事件
 	$("li.iphone",header).hover(function(){
 		$("div.iphone").toggle();
 	},function(){
@@ -9,6 +10,28 @@
 		$("div.wx").toggle();
 	},function(){
 		$("div.wx").toggle();
+	});
+	//搜索框前的搜索类别的hover和click事件
+	$(".header-b-c-t ul").hover(function(){
+		$(this).find("li:last-child").show();
+		
+	},function(){
+		$(this).find("li:last-child").hide();
+	});
+	$(".header-b-c-t li").click(function(){
+		$(this).siblings().before($(this)).hide();
+	});
+	//获取热搜数据,路径是相对于html文件
+	$.getJSON("../data/hotSearch.json",function(data){
+		$.each(data, function(index,value) {
+			$(".header-b-c-b li a").eq(index).text(value);
+		});
+	});
+	//显示购物车提示框
+	$(".header-b-r").hover(function(){
+		$(shopCarTip).toggle();
+	},function(){
+		$(shopCarTip).toggle();
 	});
 })();
 
